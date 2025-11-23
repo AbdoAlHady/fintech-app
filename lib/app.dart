4ccae4d273d29f4dcc913f0ed252f6e1881d5bd9
reem ahmad <reemmahmad40@gmail.com>
2025-11-23 20:22:48 +0200

    finish market screen ui and app router file

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:fintech_app/core/theme/app_theme.dart';
-import 'package:fintech_app/features/portfolio/screens/portfolio_intro_screen.dart';
+import 'package:fintech_app/core/utils/app_router.dart';
+import 'package:fintech_app/features/market/presentaton/views/market_screen.dart';
 import 'package:flutter/material.dart';
 
 class FintechApp extends StatelessWidget {
@@ -7,10 +8,12 @@ class FintechApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: const PortfolioIntroScreen(),
+      routerConfig: AppRouter.router,
     );
   }
 }
+ 
