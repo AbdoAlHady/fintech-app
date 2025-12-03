135fe8f53401b1eb12402b3f2d9c7f1118e01844
Abdo Al hady <20912018100332@fci.zu.edu.eg>
2025-12-03 11:49:18 +0200

    fix: Correct navigation logic in MainShellLayout for bottom navigation

--- lib/features/market/presentation/widgets/custom_bottom_nav_bar.dart
+++ lib/features/market/presentation/widgets/custom_bottom_nav_bar.dart
@@ -42,17 +42,17 @@ class _MainShellLayoutState extends State<MainShellLayout> {
           setState(() => selectedIndex = index);
           switch (index) {
             case 0:
-                context.go(AppRouter.settingsView);
+              context.go(AppRouter.homeView);
               break;
             case 1:
-                context.go(AppRouter.marketView);
+              context.go(AppRouter.marketView);
               break;
             case 2:
-                context.go(AppRouter.portfolioScreen);
+              context.go(AppRouter.portfolioScreen);
               break;
             case 3:
-               // context.go(AppRouter.portfolioScreen);
-              break;  
+              context.go(AppRouter.settingsView);
+              break;
           }
         },
         type: BottomNavigationBarType.fixed,
